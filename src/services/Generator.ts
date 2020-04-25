269dd7ad1f9c63104aa6be265d82f53f98a280b0
Piotr Moszkowicz <piotr@moszkowicz.pl>
2020-04-25 23:17:57 +0200

    Fixes to run puppeteer inside Docker

--- src/services/Generator.ts
+++ src/services/Generator.ts
@@ -7,17 +7,32 @@ import SemesterPlan from "@interfaces/SemesterPlan";
  * Generates Semester Plan and returns as BLOB
  * @param planData
  */
-export const generateSemesterPlan = async (planData: SemesterPlan): Promise<Buffer> => {
+export const generateSemesterPlan = async (
+  planData: SemesterPlan
+): Promise<Buffer> => {
   try {
     const dataBuffer = Buffer.from(JSON.stringify(planData));
     const base64encodedData = encodeURIComponent(dataBuffer.toString("base64"));
     const browser = await puppeteer.launch({
-      headless: true
+      headless: true,
+      args: [
+        // Required for Docker version of Puppeteer
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        // This will write shared memory files into /tmp instead of /dev/shm,
+        // because Docker’s default for /dev/shm is 64MB
+        "--disable-dev-shm-usage"
+      ]
     });
     const page = await browser.newPage();
-    await page.goto(`http://localhost:${config.get("app.koaPort")}/?data=${base64encodedData}`, {
-      waitUntil: "networkidle2"
-    });
+    await page.goto(
+      `http://localhost:${config.get(
+        "app.koaPort"
+      )}/?data=${base64encodedData}`,
+      {
+        waitUntil: "networkidle2"
+      }
+    );
     const buffer = await page.pdf({
       format: "A4",
       printBackground: true,
