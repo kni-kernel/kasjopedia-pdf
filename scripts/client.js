dfa20f1aad0a194632182996babe0609e6f800cf
Piotr Moszkowicz <piotr@moszkowicz.pl>
2020-04-26 14:34:30 +0200

    Add example client script to test gRPC server

---
+++ scripts/client.js
@@ -0,0 +1,24 @@
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
+
+const packageDefinition = protoLoader.loadSync("./protos/spz.proto", {keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true
+});
+const generatorProto = grpc.loadPackageDefinition(packageDefinition).spzgenerator;
+
+function main() {
+  const client = new generatorProto.SpzGeneratorService('localhost:5500', grpc.credentials.createInsecure());
+  client.generateSpz({ content: JSON.stringify({ test: true }) }, (err, response) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log("resp", response);
+  });
+}
+
+main();
